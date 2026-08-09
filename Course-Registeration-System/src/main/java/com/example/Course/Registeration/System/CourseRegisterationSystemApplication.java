b5935589f288d40cb9b01486ae36c4a8cd93e36a
NaomeP <154913950+NaomeP@users.noreply.github.com>
2026-08-09 09:47:32 +0530

    CourseRegisterationSystemApplication.java

--- Course-Registeration-System/src/main/java/com/example/Course/Registeration/System/CourseRegisterationSystemApplication.java
+++ Course-Registeration-System/src/main/java/com/example/Course/Registeration/System/CourseRegisterationSystemApplication.java
@@ -1,13 +1,56 @@
 package com.example.Course.Registeration.System;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Course.Registeration.System.model.Course;
+import com.example.Course.Registeration.System.repository.CourseRepository;
 
 @SpringBootApplication
-public class CourseRegisterationSystemApplication {
+public class CourseRegistrationSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseRegisterationSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRegistrationSystemApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner loadCourses(CourseRepository repository) {
+
+        return args -> {
+
+            if (repository.count() == 0) {
+
+                repository.save(new Course(
+                    "C101",
+                    "Java",
+                    "John",
+                    6
+                ));
+
+                repository.save(new Course(
+                    "C102",
+                    "Python",
+                    "David",
+                    8
+                ));
+
+                repository.save(new Course(
+                    "C103",
+                    "SQL",
+                    "Smith",
+                    5
+                ));
+
+                repository.save(new Course(
+                    "C104",
+                    "Web Development",
+                    "Alex",
+                    7
+                ));
+
+            }
+        };
+    }
 }
